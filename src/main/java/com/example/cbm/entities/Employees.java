85bebff8a622d94e82cbfd595eb9c17850e201cb
Ravindra08933 <134614136+Ravindra08933@users.noreply.github.com>
2023-07-12 10:07:18 +0530

    Update Employees.java

--- src/main/java/com/example/cbm/entities/Employees.java
+++ src/main/java/com/example/cbm/entities/Employees.java
@@ -9,6 +9,7 @@ import java.util.Objects;
 @NoArgsConstructor
 @ToString
 @Entity
+@Table(name="employees")
 public class Employees {
     @Id
     @Digits(integer = 10, fraction = 0, message = "Customer Number must be an integer value")
