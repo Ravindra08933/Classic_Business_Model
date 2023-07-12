ed7dd54c2d577747764df6882cc8d4a0a4a760b8
Ravindra08933 <134614136+Ravindra08933@users.noreply.github.com>
2023-07-12 10:09:51 +0530

    Update ProductLines.java

--- src/main/java/com/example/cbm/entities/ProductLines.java
+++ src/main/java/com/example/cbm/entities/ProductLines.java
@@ -11,6 +11,7 @@ import javax.persistence.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Table(name="productlines")
 public class ProductLines {
     @Id
     @GeneratedValue(generator = "randomStringGenerator")
